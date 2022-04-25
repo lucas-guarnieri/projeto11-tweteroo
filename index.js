8e1e21af16482039f03deddbb71abe3ba9a268bc
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-04-25 16:04:55 -0300

    fix - app get res method corrected

--- index.js
+++ index.js
@@ -15,9 +15,23 @@ app.post("/sing-up", (req, res) => {
 
 app.post("/tweets", (req, res) => {
     const tweet = req.body;
-    tweetsList.push(tweet);
+    tweetsList.push(tweetFormating(tweet));
     res.send("OK");
 });
+
+app.get("/tweets", (req, res) => {
+    if (tweetsList.length >= 10){
+        res.send(tweetsList);
+    }else {
+        res.send(tweetsList.slice((tweetsList.length - 5), tweetsList.length));
+    }
+});
+
+function tweetFormating(twe){
+    let message = twe.tweet
+    let header = userList.find(element => element.username === twe.username);
+    return {...header, tweet:message};
+}
 
 app.listen(5000, () => {
     console.log("Running on http://localhost:5000")
